5d0ed962f1f4700c3d8e92b596d7dce2f25bc94b
maramelasmaa <maram_4071@limu.edu.ly>
2026-02-23 14:41:54 +0200

    00

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,10 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $fillable = ['title', 'category', 'description', 'image'];
-
-    protected static function booted()
-    {
-        static::saved(fn () => \Artisan::call('export'));
-        static::deleted(fn () => \Artisan::call('export'));
-    }
 }
